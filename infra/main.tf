0a6e61ece06a3ae9d8d88f0fd3b40c6965b5d065
kwadwo-appiah <men.app32@gmail.com>
2026-02-11 10:41:19 +0000

    added terraform scripts

---
+++ infra/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+    region = "eu-west -2"
+}
+
+resource "aws_s3_bucket" {
+    bucket = "fastapi-redis-terraform-state"
+
+    tags = {
+        Name = "Terraform State Bucket"
+    }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+    bucket = aws_s3_bucket.tf_state.id
+
+    versioning_configuration {
+        status = "Enabled"
+    }
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+    name           = "terraform-lock-table"
+    billing_mode   = "PAY_PER_REQUEST"
+    hash_key       = "LockID"
+
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+}
